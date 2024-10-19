83a24ab8afa2c4cbdb5b65e32a99c0d9f70bb529
JeanC749 <100805100+JeanC749@users.noreply.github.com>
2024-10-19 03:10:45 -0600

    Script Player

--- Nimbus/Assets/Script/Personaje/PlayerMovement.cs
+++ Nimbus/Assets/Script/Personaje/PlayerMovement.cs
@@ -1,48 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f, jumpForce = 450f, wallSlideSpeed = 2f;
+    [SerializeField] private float speed = 5f, jumpForce = 12f, wallSlideSpeed = 2f,stamina = 100f;
     [SerializeField] private LayerMask groundLayer, wallLayer;
     [SerializeField] private Transform groundCheck, wallCheck;
+    
     private Rigidbody2D rb;
-    private float inputHor;
+    private float inputHor,speedRun;
+    private bool facingRight = true; // Saber si el personaje está mirando a la derecha
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        speedRun = speed * 2;
+    }
+
+    public void Stop(){
+        rb.velocity = Vector2.zero;
     }
 
     private void Update()
     {
         inputHor = Input.GetAxisRaw("Horizontal");
+        if(!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.E))
+            SetStamina(0.5f);
         Flip();
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        Run();
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() && !Input.GetKey(KeyCode.E))
             Jump();
     }
 
+    private void SetStamina(float value)
+    {
+        stamina += value;
+        if (stamina > 100)
+            stamina = 100;
+        else if (stamina < 0)
+            stamina = 0;
+    }
     private bool IsTouchingWall()
     {
-        Ray2D ray = new(wallCheck.position, Vector2.right);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 0.2f, wallLayer);
+        // Detectar si estamos tocando una pared en función de la dirección del personaje
+        Vector2 direction = facingRight ? Vector2.right : Vector2.left;
+        RaycastHit2D hit = Physics2D.Raycast(wallCheck.position, direction, 0.2f, wallLayer);
         return hit.collider != null;
     }
 
     private void Climb()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E) && stamina > 0)
         {
             float inputVer = Input.GetAxisRaw("Vertical");
-            Debug.Log($"Input Vertical: {inputVer}");
             rb.velocity = new Vector2(rb.velocity.x, inputVer * speed);
+            SetStamina(-0.5f);
         }
         else
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
     }
-
 
     private void FixedUpdate()
     {
@@ -53,12 +71,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Flip()
     {
-        if (inputHor > 0)
+        if (inputHor > 0 && !facingRight)
         {
+            facingRight = true;
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        else if (inputHor < 0)
+        else if (inputHor < 0 && facingRight)
         {
+            facingRight = false;
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
     }
@@ -66,14 +86,24 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, 0);
-        rb.AddForce(Vector2.up * (jumpForce * Time.fixedDeltaTime), ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
     }
 
     private bool IsGrounded()
     {
-        Ray2D ray = new(groundCheck.position, Vector2.down);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 0.05f, groundLayer);
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.1f, groundLayer);
         return hit.collider != null;
+    }
+
+    private void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            speed = speedRun;
+            SetStamina(-0.5f);
+        }
+        else
+            speed = 5f;
     }
 
     private void Movement()
