eb8033ddd5922a49fbe3eea15d24bf9b79e5786b
JeanC749 <100805100+JeanC749@users.noreply.github.com>
2024-10-18 22:35:00 -0600

    Word para las tareas

--- Nimbus/Assets/Script/Personaje/PlayerMovement.cs
+++ Nimbus/Assets/Script/Personaje/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private float inputHor;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        inputHor = Input.GetAxisRaw("Horizontal");
+        Flip();
+        if(Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void FixedUpdate() {
+        Movimiento();
+    }
+
+    private void Flip() {
+        if (inputHor > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (inputHor < 0) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+    
+    private void Jump() {
+        Debug.Log("Jump");
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * (jumpForce * Time.fixedDeltaTime), ForceMode2D.Impulse);
+    } 
+
+    private void Movimiento() {
+        rb.velocity = new Vector2(inputHor * speed, rb.velocity.y);
     }
 }
